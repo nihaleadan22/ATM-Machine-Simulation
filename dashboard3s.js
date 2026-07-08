5daff88526d7e6f40a882ee499b6c4bb9e986485
nihaleadan22 <adannn484@gmail.com>
2026-07-08 02:30:05 -0700

    Add files via upload

--- dashboard3s.js
+++ dashboard3s.js
@@ -1,100 +1,121 @@
+//==============================
+// ATM Dashboard 
+//==============================
+
+// Buttons
 const checkBalanceBtn = document.querySelector("#checkBalance");
 const depositBtn = document.querySelector("#depositMoney");
 const withdrawBtn = document.querySelector("#withdrawMoney");
 const changePinBtn = document.querySelector("#changePin");
 const exitBtn = document.querySelector("#exit");
-
-//input
-const amountInput = document.querySelector("#amount");
 const confirmBtn = document.querySelector("#confirmBtn");
-// display values
+const historyBtn = document.querySelector("#historyBtn");
+// Input
+const amountInput = document.querySelector("#amount");
+// Display
 const balanceAmount = document.querySelector("#balanceAmount");
 const statusText = document.querySelector("#status");
-const historyBtn = document.querySelector("#historyBtn");
 const transactionList = document.querySelector("#transactionList");
-
-
-let correctPin = "1234";
-let cardInserted = false;
+// Variables
 let balance = 50000;
+let correctPin = "1234";
 let transactionHistory = [];
+let currentOperation = "";
+// Event Listeners
+checkBalanceBtn.addEventListener("click", checkBalance);
+depositBtn.addEventListener("click", showDepositSection);
+withdrawBtn.addEventListener("click", showWithdrawSection);
+confirmBtn.addEventListener("click", confirmTransaction);
+changePinBtn.addEventListener("click", changePin);
+historyBtn.addEventListener("click", displayTransactions);
+exitBtn.addEventListener("click", exitATM);
 
-
-//check  balaance
-function checkBalance(){
-balanceAmount.textContent = 'Rs ${balance}';
+// Check Balance
+function checkBalance() {
+balanceAmount.textContent = `Rs. ${balance}`;
 statusText.textContent = "Current balance displayed successfully.";
 }
 
-//event listner
-checkBalanceBtn.addEventListener("click",checkBalance);
-depositBtn.addEventListener("click", showDepositSection);
-withdrawBtn.addEventListener("click", showWithdrawSection);
-changePinBtn.addEventListener("click", changePin);
-confirmBtn.addEventListener("click", confirmTransaction);
-exitBtn.addEventListener("click", exitATM);
-historyBtn.addEventListener("click", displayTransactions);
-historyBtn.addEventListener("click", displayTransactions);
+// Deposit
+function showDepositSection() {
+currentOperation = "deposit";
+statusText.textContent = "Enter amount and click Confirm.";
+}
 
-//deposit money function
-function depositMoney(){
-let amount = Number(amountInput.value);
-if(amount <= 0){
-statusText.textContent = "Please enter a valid amount.";
+// Withdraw
+function showWithdrawSection() {
+currentOperation = "withdraw";
+statusText.textContent = "Enter amount and click Confirm.";
+}
+
+// Confirm Transaction
+function confirmTransaction() {
+let enteredAmount = Number(amountInput.value);
+if (amountInput.value === "") {
+statusText.textContent = "Please enter an amount.";
 return;
 }
-balance += amount;
-balanceAmount.textContent = `Rs. ${balance}`;
-statusText.textContent = "Money deposited successfully.";
-transactionHistory.push(`Deposited Rs. ${amount}`);
-saveData();
-amountInput.value= "";
-}
-depositMoney();
-
-//withdraw money
-function showWithdrawSection() {
-let enteredAmount = Number(amountInput.value);
 if (enteredAmount <= 0) {
-statusText.textContent = "Please enter a valid amount.";
-saveData();
+statusText.textContent = "Amount must be greater than zero.";
 return;
     }
-if (enteredAmount <= balance) {
-balance -= enteredAmount;
-balanceAmount.textContent = `Rs. ${balance}`;
-statusText.textContent = "Please collect your cash.";
-amountInput.value = "";
-}
-else {
+if(!Number.isInteger(enteredAmount)) {
+statusText.textContent = "Decimal values are not allowed.";
+return;
+    }
+if (enteredAmount > 500) {
+statusText.textContent = "Maximum transaction limit is Rs. 500.";
+return;
+    }
+if (currentOperation === "deposit") {
+balance += enteredAmount;
+transactionHistory.push(`Deposited Rs. ${enteredAmount}`);
+statusText.textContent = "Money deposited successfully.";
+    }
+else if (currentOperation === "withdraw") {
+if (enteredAmount > balance) {
 statusText.textContent = "Insufficient Balance!";
-}
-}
-
-//transaction history
-function confirmTransaction() {
-let enteredAmount=Number(amountInputvalue);
-if(enteredAmount<=0){
-statusText.textContent = "Please enter a valid amount."
 return;
 }
+balance -= enteredAmount;
+transactionHistory.push(`Withdrawn Rs. ${enteredAmount}`);
+statusText.textContent = "Please collect your cash.";
 }
+else {
+statusText.textContent = "Please select Deposit or Withdraw first.";
+return;
+    }
+balanceAmount.textContent = `Rs. ${balance}`;
+saveData();
+displayTransactions();
+amountInput.value = "";
+}
+//display transactionn
 function displayTransactions() {
+if (transactionList.style.display === "none") {
+transactionList.style.display = "block";
+}
+else {
+transactionList.style.display = "none";
+ return;
+    }
 transactionList.innerHTML = "";
 transactionHistory.forEach(function(transaction) {
 transactionList.innerHTML += `
 <li>${transaction}</li>
- `;
+        `;
 });
 }
-
 //change pin
 function changePin() {
-let newPin = prompt("Enter New PIN");
-if (newPin.length !== 4) {
-statusText.textContent = "PIN must be 4 digits.";
+let newPin = prompt("Enter New 4-digit PIN");
+if (newPin === null) {
+        return;
+    }
+if (newPin.length !== 4 || isNaN(newPin)) {
+statusText.textContent = "PIN must be exactly 4 digits.";
 return;
-}
+    }
 correctPin = newPin;
 saveData();
 statusText.textContent = "PIN changed successfully.";
@@ -103,15 +124,11 @@ function exitATM() {
 alert("Thank you for using our ATM.");
 window.location.href = "index.html";
 }
-
-//save function // Save Data
+// Save Data
 function saveData() {
 localStorage.setItem("balance", balance);
 localStorage.setItem("pin", correctPin);
-localStorage.setItem(
-     "history",
-JSON.stringify(transactionHistory)
-);
+localStorage.setItem("history", JSON.stringify(transactionHistory));
 }
 // Load Data
 function loadData() {
@@ -126,62 +143,12 @@ correctPin = savedPin;
     }
 if (savedHistory !== null) {
 transactionHistory = JSON.parse(savedHistory);
-    }
+}
 balanceAmount.textContent = `Rs. ${balance}`;
-displayTransactions();
-}
-//display transaction
-function displayTransactions() {
-transactionList.innerHTML = "";
-transactionHistory.forEach(function(transaction) {
-transactionList.innerHTML += `
-<li>${transaction}</li>
-        `;
-    });
-}
-
-let currentOperation = "";
-function showDepositSection() {
-    currentOperation = "deposit";
-    statusText.textContent = "Enter amount and click Confirm.";
-}
-function showWithdrawSection() {
-    currentOperation = "withdraw";
-    statusText.textContent = "Enter amount and click Confirm.";
-}
-function confirmTransaction() {
-let enteredAmount = Number(amountInput.value);
-// Validate amount
-if (enteredAmount <= 0) {
-        statusText.textContent = "Please enter a valid amount.";
-        return;
-    }
-// Deposit
-if (currentOperation === "deposit") {
-balance += enteredAmount;
-transactionHistory.push(`Deposited Rs. ${enteredAmount}`);
-statusText.textContent = "Money deposited successfully.";
-    }
-// Withdraw
-else if (currentOperation === "withdraw") {
-if (enteredAmount > balance) {
-statusText.textContent = "Insufficient Balance!";
-return;
-        }
-balance -= enteredAmount;
-transactionHistory.push(`Withdrawn Rs. ${enteredAmount}`);
-statusText.textContent = "Please collect your cash.";
-    }
- // No operation selected
-else {
-statusText.textContent = "Please select Deposit or Withdraw first.";
-return;
-    }
-balanceAmount.textContent = `Rs. ${balance}`;
-saveData();
-displayTransactions();
-amountInput.value = "";
+transactionList.style.display = "none";
 }
 loadData();
-
+localStorage.removeItem("balance");
+localStorage.removeItem("pin");
+localStorage.removeItem("history");
 
